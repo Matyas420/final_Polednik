a9197fc75969b00e1f11aad81cb847cedd46c5b4
Matyas420 <159123612+Matyas420@users.noreply.github.com>
2024-10-09 11:23:36 +0200

    index.py

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import random
 #1. V poli je 9 hodnot od 0 do 100
-
 array1 = [25, 14, 56, 32, 47, 75, 81, 96, 67]
-#2. Print vypíše čísla na indexech "0, 3, 8"
 
+#2. Print vypíše čísla na indexech "0, 3, 8"
 print(array1[0])
 print(array1[len(array1) // 2])
 print(array1[-1])
+
 #3. Zamění 5 indexovou hodnotu na 34
 array1[5] = 34
 
